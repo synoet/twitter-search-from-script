db7286760ffec64e037736bfadb1ff01f3e47ef7
synoet <teonys@nyu.edu>
2022-11-23 03:33:29 -0500

    FIX from: , store click in input and redirect

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-let delay = (time) => new Promise((resolve) => setTimeout(resolve, time));
 const input = document.querySelectorAll(
   "[data-testid=SearchBox_Search_Input]"
 )[0];
+
+let selectedValues = [];
+
+input.addEventListener("click", (event) => {
+  if (selectedValues.length > 0) {
+    event.target.value = selectedValues.join(" ");
+  }
+});
+
+input.addEventListener("keydown", (event) => {
+  if (event.key === "Enter" && selectedValues.length > 0) {
+    window.location =
+      "https://twitter.com/search?q=" +
+      selectedValues.join(" ") +
+      "&src=typed_query&f=top";
+    event.stopPropagation();
+  }
+});
+
 input.addEventListener("keydown", (event) => {
   if (
     event.key === "Backspace" &&
@@ -11,8 +29,8 @@ input.addEventListener("keydown", (event) => {
     input.parentElement.children[1].remove();
     input.style.paddingLeft = "0px";
   }
-  if (event.target.value.includes(":from")) {
-    event.target.value = event.target.value.replace(":from", "");
+  if (event.target.value.includes("from:")) {
+    event.target.value = event.target.value.replace("from:", "");
     let p = document.createElement("p");
     input.parentElement.style.position = "relative";
     p.style.position = "absolute";
@@ -31,12 +49,6 @@ input.addEventListener("keydown", (event) => {
 
     if (input.parentElement.children.length === 2) {
       searchResults.forEach((element) => {
-        console.log(
-          element.getElementsByClassName(
-            "css-1dbjc4n r-1awozwy r-18u37iz r-1v8amoe"
-          ).length
-        );
-
         if (
           element.children[0].children[0].className !==
           "css-18t94o4 css-1dbjc4n r-1ny4l3l r-ymttw5 r-1f1sjgu"
@@ -49,6 +61,23 @@ input.addEventListener("keydown", (event) => {
         ) {
           element.children[0].remove();
         }
+
+        const setUsernameInInput = (event) => {
+          input.value =
+            (input.value.includes("@") ? input.value : "") +
+            ` ${
+              element.getElementsByClassName(
+                "css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0"
+              )[2].innerText
+            }`;
+          selectedValues.push(
+            element.getElementsByClassName(
+              "css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0"
+            )[2].innerText
+          );
+          event.stopPropagation();
+        };
+        element.addEventListener("click", setUsernameInInput);
       });
     }
   });
